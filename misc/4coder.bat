6a3f3ed67c0f22e0251299e664b853fcb2d54377
Ryan Fleury <ryan.j.fleury@gmail.com>
2018-05-03 19:35:51 -0600

    Added 4coder stuff

---
+++ misc/4coder.bat
@@ -0,0 +1,2 @@
+@echo off
+call C:\Users\Ryan\Desktop\tools\4coder\4ed.exe
---
+++ misc/shell.bat
@@ -0,0 +1,3 @@
+@echo off
+call R:\"Visual Studio"\VC\vcvarsall.bat x64
+::call C:\"Program Files (x86)"\"Microsoft Visual Studio 14.0"\VC\vcvarsall.bat x64
