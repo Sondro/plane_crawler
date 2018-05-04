d16cb197fa11be0e04f9014646d9c1660c3d9ac1
Ryan Fleury <ryan.j.fleury@gmail.com>
2018-05-03 20:27:20 -0600

    Added build_and_run.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
+del plane_crawler.exe
 set compiler_flags= -Wall -g -msse4.1 -std=c++11
 set linker_flags= -lglfw3 -lopengl32 -lgdi32 -lopenal32 
-
 g++ %compiler_flags% ./source/main.cpp -I ./source/ %linker_flags% -o plane_crawler.exe
